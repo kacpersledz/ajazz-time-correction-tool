7154be5f23eada25b45f8a9a59fe7aad2aabcf79
kacpersledz <casper.sledx@gmail.com>
2026-09-22 18:24:55 +0200

    feat: add Nix flake package

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Nix flake for ajazz-time-correction-tool";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+      package = pkgs.rustPlatform.buildRustPackage {
+        pname = cargoToml.package.name;
+        version = cargoToml.package.version;
+
+        src = self;
+
+        cargoLock.lockFile = ./Cargo.lock;
+
+        nativeBuildInputs = [
+          pkgs.pkg-config
+        ];
+
+        buildInputs = [
+          pkgs.udev
+        ];
+
+        meta = {
+          description = "Synchronize the clock of an Ajazz AK820 keyboard";
+          homepage = "https://github.com/kacpersledz/ajazz-time-correction-tool";
+          license = pkgs.lib.licenses.mit;
+          mainProgram = "ajazz-time-correction-tool";
+          platforms = pkgs.lib.platforms.linux;
+        };
+      };
+    in
+    {
+      packages.${system} = {
+        default = package;
+        ajazz-time-correction-tool = package;
+      };
+
+      checks.${system}.package = package;
+    };
+}
